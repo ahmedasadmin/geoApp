6a3de6262bda042baddb6bc1bfd93e7634bdc56e
ahmedasadmin <ahmednagi.hawk@gmail.com>
2024-02-26 09:36:42 +0200

    First commit

---
+++ mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QXmlStreamReader>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QNetworkAccessManager* addressRequest;
+private slots:
+    void getAddressFinished(QNetworkReply*);
+    void on_pushButton_clicked();
+};
+#endif // MAINWINDOW_H
